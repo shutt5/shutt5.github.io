cefc2ef65cfd4e069c1dc08a89fb38d1234b3208
Stephen Hutt <shutt@ucsc.edu>
2015-10-11 17:31:52 -0400

    Trying New Things

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<?
+include 'nav.php';
+?>
+
+<body>
+<div class="container">
+  <h1> Placating Plato with Plates of Pasta </h1>
+  <p> Welcome to our new online tool.</p>
+  <br>
+  <h4> What is Placating Plato? </h4>
+  <p> Explain </p>
+  <br>
+  <h4> Do I need an account? </h4>
+  <p> Explain </p>
+  <h4> How do I get Started\? </h4>
+  <p> Explain </p>
+
+</div>
+</body>
+</html>
