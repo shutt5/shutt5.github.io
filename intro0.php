c243868668ef8f42eb487d681abe3a0b5d92b630
Stephen Hutt <shutt@ucsc.edu>
2015-10-21 17:50:44 -0400

    Who are we

---
+++ intro0.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+<body>
+
+<div class="container">
+
+
+  <?
+  include 'nav.php';
+  ?>
+
+<h2>Introduction</h2>
+<p>The Dining Philosophers Problem is a popular way of thinking about concurrency issues in computers.  Throughout this lesson you will learn how this problem is at work behind the scenes every time you use your computer.<br>
+<br>
+ALRIGHT!<br>
+<br>
+Suppose we have five philosophers sitting around a table.  Each philosopher has a plate of food in front of them.  Between each pair of philosophers is a single chopstick.  How do we get the philosophers to share the chopsticks without communicating and such that no philosopher starves?<br>
+<br>
+</p>
+
+<p>INSERT PRETTY PICTURES HERE</p>
+
+<p> Main problems to avoid:<br>
+<br>
+Deadlock:<br>
+</p>
+
+<p> INSERT ANIMATION OF PHILOSOPHERS DINING HERE</p>
+
+<p> Starvation: <br></p>
+
+<p> INSERT ANIMATION OF PHILOSOPHERS DINING HERE</p>
+
+<p> Continue on to learn about this junk. </p>
+</div>
+</body>
+</html>
