9d68f5438ecfed0fe1d54d5bd669a22a2e65ef57
Stephen Hutt <shutt@ucsc.edu>
2015-11-08 17:36:05 -0500

    over.php

--- code.php
+++ code.php
@@ -3,10 +3,7 @@
 <link rel="stylesheet" href="css/bootstrap.css">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 
 <body>
---
+++ over.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.css">
+<?
+include 'nav.php';
+
+?>
+
+<body>
+<div class="container">
+
+</div>
+</body>
+</html>
