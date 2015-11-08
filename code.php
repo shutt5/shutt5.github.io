084c8932d3069763e2eb30b6576dce02f91a84da
nkremerh <nkremerh@nd.edu>
2015-11-08 17:47:14 -0500

    Modifies textarea girth again

--- code.php
+++ code.php
@@ -28,7 +28,7 @@ If you are not too confident in your JavaScript abilities, please refer to the <
 <div>
 Code Here:
 <form>
-<textarea id="code" cols="64" rows="64">
+<textarea id="code" cols="64" rows="128">
 function dining_philosophers_problem() {
 
 	var blobURL = URL.createObjectURL(new Blob(['(',
