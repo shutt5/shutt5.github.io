2683a6b3bba0f2ef3feaa93fd4d4ac4b1da727e3
nkremerh <nkremerh@nd.edu>
2015-11-12 17:02:34 -0500

    Adds prototype thank you page for use at the end of the tool

---
+++ thankyou.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<div>
+<p>
+Thank you for using our tool. We hope you found it more enjoyable than staring at a textbook. Please complete the post-test to show how much you learned. Then, fill out our survey to let us know how we can make this tool even more enjoyable and easier to use. Your input is appreciated.
+</p>
+</div>
+</body>
+</html>
