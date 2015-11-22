edfc7f3cba3b8bd8c263ac67f9796489b593778f
ljfaust2 <lfaust@nd.edu>
2015-11-22 18:16:30 -0500

    changed wording on stuff

--- PPPP/realWorld.php
+++ PPPP/realWorld.php
@@ -21,29 +21,29 @@
 <h2> So we've seen the problem... </h2>
   But what does it mean?
 
-  The dining philosphers is an interesting representation of a problem. A problem that is directly transferrable to Operating Systems.
+  The Dining Philosphers Problem is an interesting representation of a concurrency problem in Operating Systems.
   There are many different guises of the problem, for example
 
   <? include 'example.html' ?>
 
-  <h2> But what does this mean for Operating Systems </h2>
+  <h2> But what does this mean for operating systems? </h2>
 
   In an operating systems situation we can think of the following:
 
   <ul>
-    <li> Philosophers are Processes</li>
-    <li> The chopsticks are the resources that the processes need for example certain files</li>
-    <li> All the processes need to run correctly</li>
+    <li> Philosophers are processes</li>
+    <li> Chopsticks are resources that the processes need, for example, certain files</li>
+    <li> All processes need to run correctly</li>
  </ul>
 
-The problems that exist for the philosphers also exist for operating systems.
+These problems have parallels for operating systems.
 
  <ul>
-    <li> Deadlock would mean nothing would run, process would not be completed </li>
+    <li> Deadlock would mean nothing would run or processes would not complete </li>
     <li> Starvation would mean certain porcesses would never finish </li>
 </ul>
 
-Either of these conditions would result in the Operating system failing to perform it's task.
+Either of these conditions would result in the operating system failing to perform it's task.
 
 So how do we solve it?
 
