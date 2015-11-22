75055c67446a25e1c8300a4093b87987dcb88dd2
Stephen Hutt <shutt@ucsc.edu>
2015-11-22 17:43:51 -0500

    Update Index

--- PPPP/thankyou.php
+++ PPPP/thankyou.php
@@ -22,8 +22,21 @@
 <p>
 Thank you for using our tool, we hope you have enjoyed working through Placating Plato with Plates of Pasta,
 
-It is now time for you to take a quick survery to show us what you have learned. Please click the link below to be taken to the poost test</p>
+It is now time for you to take a quick survery to show us what you have learned. Please click the link below to be taken to the post test</p>
 </div>
+
+<div class="row">
+
+  <div class="row">
+    <div class="col-md-10"></div>
+    <div class="col-md-2">
+      <a href = "https://docs.google.com/a/nd.edu/forms/d/1OTopbTQUTcmQVC2lMnLaBSMEG5PecwHqEg_KIUklL2s/viewform"><button type="button" class="btn btn-lg btn-primary">Complete!</button></a>
+    </div>
+  </div>
+
+</div>
+
+
 </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -29,7 +29,22 @@
               <h3 class="panel-title">Welcome</h3>
             </div>
             <div class="panel-body">
+              Thank you for agreeing to take part in our study.
+              The study consists.of three sections.
 
+              <ul>
+                <li> An initial survey </li>
+                <li> An interactive learning session </li>
+                <li> A short exit survey </li>
+              </ul>
+
+
+              This study is part of the Graduate Operating Systems course at the University of Notre Dame (CSE 60461).
+              If at any time you have any questions, please do not hesitate to get in touch. you can email any of the group members
+              listed below.
+
+              If halfway through the study you decide that you do not wish to participate, simply close the window, we will discard any data
+              from the initial survey that does not match to data from the post survey.
             </div>
           </div>
 
@@ -38,7 +53,10 @@
             <h3 class="panel-title">What will happen to my data?</h3>
           </div>
           <div class="panel-body">
-
+            Data collected in this study will be used only in the context of the Graduate Operating Systems Course at the University of Notre Dame.
+            Your Notre Dame username will be collected as part of the study, this will only be used to match data together and will not be used in
+            conjuction with your answers.
+            No data in our final write up will involve your usernames and will all be de-personalized.
           </div>
         </div>
 
@@ -48,7 +66,7 @@
             <h3 class="panel-title">How long will it take?</h3>
           </div>
           <div class="panel-body">
-
+            You can take as long as you like, but we imagine you will spend around 20-30 minutes.
           </div>
         </div>
 
@@ -60,37 +78,26 @@
           <h3 class="panel-title">Summary</h3>
         </div>
         <div class="panel-body">
-
+          <ul>
+            <li>Clicking begin will take you too a short survey </li>
+            <li>Then click the link at the end of the survey go through the study <li>
+            <li>At the end of the study take another survey</li>
+            <li>Data from this study will only be used in the context of the Graduate Operating Systems Class at the University of Notre Dame </li>
+            <li>Your usename is stored to match data from the two tests, it will not be used in conjuction with the data </li>
+          </ul>
         </div>
 
       </div>
 
 
 
- <!--        <div class="panel panel-primary">
-                      <div class="panel-heading">
-                        <h3 class="panel-title">Do I need an account?</h3>
-                      </div>
-                      <div class="panel-body">
-                        No, you can run through our tutorial without an account. Just click 'Get Started' to get underway.
-                      </div>
-                    </div>*/
-
-    <div class="panel panel-primary">
-        <div class="panel-heading">
-            <h3 class="panel-title">How do I get started?</h3>
-        </div>
-        <div class="panel-body">
-            It's easy, you just click 'Get Started' which is obvious, so you probably are not reading this.
-        </div>
-    </div>-->
 
       <div class="row">
 
         <div class="row">
           <div class="col-md-10"></div>
           <div class="col-md-2">
-            <a href = "intro.php"><button type="button" class="btn btn-lg btn-primary">Begin!</button></a>
+            <a href = "https://docs.google.com/a/nd.edu/forms/d/1o_XJFPATvyWi-82IVFHGMXgSkecsyuQdh7bOrdavbDE/viewform"><button type="button" class="btn btn-lg btn-primary">Begin!</button></a>
           </div>
         </div>
 
