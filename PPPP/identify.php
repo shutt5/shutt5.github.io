289e3445907796a5eb3d4220b53148d54ffc4ef1
nkremerh <nkremerh@nd.edu>
2017-01-27 14:22:44 -0500

    Adds indentify page

---
+++ PPPP/identify.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+<body>
+<div class="container">
+
+
+  <?
+  include 'nav.php';
+  $SESSION['started'] = "t";
+  ?>
+
+
+
+<div class="row">
+
+<div class="row">
+  <div class="col-md-10"></div>
+  <div class="col-md-2">
+    <a href = "code.php"><button type="button" class="btn btn-lg btn-primary">Next</button></a>
+  </div>
+</div>
+
+</div>
+</body>
+</html>
