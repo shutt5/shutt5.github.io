c5c6e58104e0e39408ed6b92262aaaef04db6238
liuanqin <aliu1@nd.edu>
2017-01-27 11:07:42 -0500

    example revised

---
+++ PPPP/example.php
@@ -0,0 +1,29 @@
+	<title></title>
+<title></title>
+</head>
+<body>
+<h2>Real Life Example&nbsp;</h2>
+
+<p>ND has the best football program in the whole country. But during their first season in 1887, Notre Dame could not afford enough jerseys and helmets for each player. There were 100 players on the team, but with only 50 helmets and jerseys available. Each player needs to put on a helmet and a jersey in order to play.</p>
+
+<p>Why would it be bad:</p>
+
+<ul>
+	<li>There could be only 50 people at most playing at the same time.</li>
+	<li>If someone played for a lot of time, then others would have limited playing time.</li>
+	<li>Anyone Could not enter the game without a helmet or jersey.</li>
+	<li>Deadlock happened if someone had only a helmet or a jersey.&nbsp;</li>
+	<li>Starvation happened when no more helmets or jerseys were available.</li>
+</ul>
+
+<p>Soultion:</p>
+
+<ul>
+	<li>We can set up a rule that each player plays for a certain amount of time then gives his gear to another person; this rotation then repeats.&nbsp;</li>
+	<li>If a player only has a helmet, he needs to pass his helmet on to someone who only has a jersey to avoid deadlock.&nbsp;</li>
+	<li>Every ND player should play hard and respectfully.&nbsp;</li>
+</ul>
+
+<p><img alt="" src="http://nocoastbias.com/wp-content/uploads/2012/08/NDFootball.jpg" style="width: 400px; height: 280px;" /></p>
+</body>
+</html>
