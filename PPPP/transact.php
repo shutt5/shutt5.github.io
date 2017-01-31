39de1d7b94c8262e77e6cf7cc8f98e74e3ed0b2b
Stephen Hutt <shutt@nd.edu>
2017-01-31 16:45:43 -0500

    Update transact

--- PPPP/transact.php
+++ PPPP/transact.php
@@ -4,12 +4,12 @@
 $Participant = "Test";
 
 $Participant = $_GET["PID"];
-
+$PClean = str_replace(':', '', $Participant);
 $StringToWrite = $_GET["string"];
 $StringToWrite =  $StringToWrite ."," . date("Y-m-d h:i:sa") . "\n";
 $filePath = "";
 
-$file = $filePath . $Participant . "-Transaction.txt";
+$file = $filePath . $PClean . "-Transaction.txt";
 
 if(file_exists($file)){
   $fh = fopen($file, 'a') or die("can't open file");
