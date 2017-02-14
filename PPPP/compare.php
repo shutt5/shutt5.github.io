300277f5e267ccfb2ad3e27a2fb9ba264679fa91
jmathishd436 <jdebened@nd.edu>
2017-02-13 20:10:33 -0500

    Update compare.php
    Now colors?

--- PPPP/compare.php
+++ PPPP/compare.php
@@ -151,7 +151,7 @@ function showAnswer(num) {
 	switch(num) {
 		case 1:
 			show = document.getElementById('answer1');
-			show.setAttribute("style", "");
+			show.setAttribute("style", "background-color:#97cc04");
 			hide1 = document.getElementById('answer2');
 			hide1.setAttribute("style", "display: none");
 			hide2 = document.getElementById('answer3');
@@ -159,7 +159,7 @@ function showAnswer(num) {
 			break;
 		case 2:
 			show = document.getElementById('answer2');
-			show.setAttribute("style", "");
+			show.setAttribute("style", "background-color:#f45d01");
 			hide1 = document.getElementById('answer1');
 			hide1.setAttribute("style", "display: none");
 			hide2 = document.getElementById('answer3');
@@ -321,7 +321,7 @@ Function philosopher{<br>
 	 <div class="col-md-6">
 
 <button type="button" class="btn btn-lg btn-primary" onclick="showAnswer(1)">Code 1</button>
-<p id="answer1" class="border" style="background-color:#97cc04; display: none">
+<p id="answer1" class="border" style="display: none">
 Correct! Since the sticks are numbered, we can avoid deadlock by making sure not all five philosophers are holding one chopstick. We also avoid starvation with the timer on eating preventing a single philosopher from eating for too long.
 </p>
 
@@ -329,7 +329,7 @@ Correct! Since the sticks are numbered, we can avoid deadlock by making sure not
 <div class="col-md-6">
 
 <button type="button" class="btn btn-lg btn-primary" onclick="showAnswer(2)">Code 2</button>
-<p id="answer2" class="border" style="background-color:#f45d01; display: none">
+<p id="answer2" class="border" style="display: none">
 Incorrect. Since philosophers eat until full, we may encounter starvation in which philosophers starve waiting for one philosopher to eat their fill. We also may encounter deadlock since we place no restrictions on which chopsticks to pick up, thus each philosopher may pick up one chopstick and all be stuck waiting on the other chopstick.
 </p>
 </div>
